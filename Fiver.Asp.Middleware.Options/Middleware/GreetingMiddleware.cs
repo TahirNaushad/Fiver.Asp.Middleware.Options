079d9ad64c9410c43b72982f59da1423e11a96e5
Tahir Naushasd <t.naushad25@gmail.com>
2017-08-29 11:26:35 +0100

    separated methods for the post

--- Fiver.Asp.Middleware.Options/Middleware/GreetingMiddleware.cs
+++ Fiver.Asp.Middleware.Options/Middleware/GreetingMiddleware.cs
@@ -3,13 +3,34 @@ using System.Threading.Tasks;
 
 namespace Fiver.Asp.Middleware.Options
 {
+    //public class GreetingMiddleware
+    //{
+    //    private readonly RequestDelegate next;
+    //    private readonly GreetingOptions options;
+
+    //    public GreetingMiddleware(
+    //        RequestDelegate next,
+    //        GreetingOptions options)
+    //    {
+    //        this.next = next;
+    //        this.options = options;
+    //    }
+
+    //    public async Task Invoke(
+    //        HttpContext context)
+    //    {
+    //        var message = $"Good {this.options.GreetAt} {this.options.GreetTo}";
+    //        await context.Response.WriteAsync(message);
+    //    }
+    //}
+
     public class GreetingMiddleware
     {
         private readonly RequestDelegate next;
         private readonly GreetingOptions options;
 
         public GreetingMiddleware(
-            RequestDelegate next, 
+            RequestDelegate next,
             GreetingOptions options)
         {
             this.next = next;
--- Fiver.Asp.Middleware.Options/Startup.cs
+++ Fiver.Asp.Middleware.Options/Startup.cs
@@ -6,30 +6,39 @@ namespace Fiver.Asp.Middleware.Options
 {
     public class Startup
     {
+        //public void ConfigureServices(
+        //    IServiceCollection services)
+        //{
+        //    services.AddMessageFormatter(new MessageOptions
+        //    {
+        //        Format = MessageFormat.Lower
+        //    });
+        //}
+
         public void ConfigureServices(
             IServiceCollection services)
         {
-            //services.AddMessageFormatter(new MessageOptions
-            //{
-            //    Format = MessageFormat.Lower
-            //});
-
             services.AddMessageFormatter(options =>
             {
                 options.Format = MessageFormat.Upper;
             });
         }
 
+        //public void Configure(
+        //    IApplicationBuilder app,
+        //    IHostingEnvironment env)
+        //{
+        //    app.UseGreeting(new GreetingOptions
+        //    {
+        //        GreetAt = "Morning",
+        //        GreetTo = "Tahir"
+        //    });
+        //}
+
         public void Configure(
-            IApplicationBuilder app, 
+            IApplicationBuilder app,
             IHostingEnvironment env)
         {
-            //app.UseGreeting(new GreetingOptions
-            //{
-            //    GreetAt = "Morning",
-            //    GreetTo = "Tahir"
-            //});
-
             app.UseGreeting(options =>
             {
                 options.GreetAt = "Evening";
